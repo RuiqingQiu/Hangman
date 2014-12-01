274170bb57a6bc567943627e6d83afeb9f2d9f4a
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2014-12-01 01:25:31 -0800

    graph and bug fixed

---
+++ toUpper.py
@@ -0,0 +1,22 @@
+import random
+count = 0
+random_num_list = []
+write_file = open("upper_word",'w')
+with open ('words') as File:
+    for line in File:
+        count += 1
+for i in range(0, 2000):
+    random_num_list.append(random.randint(0, count))
+no_duplicate_list = list(set(random_num_list))
+no_duplicate_list.sort()
+count = 0
+index = 0
+with open ('words') as File:
+    for line in File:
+        if count == no_duplicate_list[index]:
+            write_file.write(line.upper())
+            index += 1
+            if index == len(no_duplicate_list):
+                break
+        count += 1
+
