0a5ee60c101f093ba2b848ded26a3dc9ac2333a0
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2014-12-08 11:31:42 -0800

    barchart added. need to remove word graph

--- toUpper.py
+++ toUpper.py
@@ -1,11 +1,12 @@
 import random
 count = 0
 random_num_list = []
+
 write_file = open("upper_word",'w')
 with open ('words') as File:
     for line in File:
         count += 1
-for i in range(0, 2000):
+for i in range(0, 10000):
     random_num_list.append(random.randint(0, count))
 no_duplicate_list = list(set(random_num_list))
 no_duplicate_list.sort()
@@ -20,3 +21,10 @@ with open ('words') as File:
                 break
         count += 1
 
+"""
+write_file = open("upper_words",'w')
+with open ('words') as File:
+    for line in File:
+        write_file.write(line.upper())
+print "Done"
+"""
