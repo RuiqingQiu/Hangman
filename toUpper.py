5f886a0713e2827c93152e16d1cac2a507c71922
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2014-12-15 02:48:15 -0800

    Added game counter, fixed some bugs

--- toUpper.py
+++ toUpper.py
@@ -1,12 +1,12 @@
 import random
 count = 0
 random_num_list = []
-
+"""
 write_file = open("upper_word",'w')
 with open ('words') as File:
     for line in File:
         count += 1
-for i in range(0, 10000):
+for i in range(0, 80000):
     random_num_list.append(random.randint(0, count))
 no_duplicate_list = list(set(random_num_list))
 no_duplicate_list.sort()
@@ -22,9 +22,9 @@ with open ('words') as File:
         count += 1
 
 """
-write_file = open("upper_words",'w')
-with open ('words') as File:
+write_file = open("common",'w')
+with open ('common_words') as File:
     for line in File:
         write_file.write(line.upper())
 print "Done"
-"""
+
